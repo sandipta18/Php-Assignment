5abebcb8c8262aa57ca87edef3e5ec711924d622
sandipta18 <sandipta.sardar@innoraft.com>
2023-02-20 14:00:27 +0530

    FT2023-55: Implemented Assignment 7 with OOPS .

--- Assignment-7/index.php
+++ Assignment-7/index.php
@@ -20,46 +20,62 @@ ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../s
 
   //This is checking whether the username and passowrd is empty or not
   //and will check if the username and passowrd is correct or not
-  $errorname = "";
-  $errorpassword = "";
-  $user_name = $_POST["Name"];
-  $password = $_POST["Password"];
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["Name"];
-    $pass = $_POST["Password"];
-    //If name is empty throw error
-    if (empty($_POST["Name"])) {
-      $errorname = "Please Enter Name";
-    }
-    //IF password is empty throw error
-    if (empty($_POST["Password"])) {
-      $errorpassword = "Please Enter Password";
-    }
-    //If username is wrong throw error
-    if ($user_name != "sandipta") {
-      $errorname = "Enter User Name Correctly";
-    }
-    //If  password is wrong throw error
-    if ($password != "innoraft") {
-      $errorpassword = "Enter Password Correctly";
-    }
-    //if username and passwor is correct redirect to action page
-    if ($user_name === "sandipta18" || $user_name === "indra" || $user_name == "rajdip" && $password === "admin") {
-      $_SESSION['name'] = ucwords(strtolower($_POST['Name']));
-      header("Location:action.php");
-    }
 
+  class Login
+  {
+    function validate_login()
+    {
+      global $errorname;
+      global $errorpassword;
+      global $user_name;
+      global $password;
+      $errorname = "";
+      $errorpassword = "";
+      $user_name = $_POST["Name"];
+      $password = $_POST["Password"];
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //If name is empty throw error
+        if (empty($_POST["Name"])) {
+          $errorname = "Please Enter Name";
+        }
+        //IF password is empty throw error
+        if (empty($_POST["Password"])) {
+          $errorpassword = "Please Enter Password";
+        }
+        //If username is wrong throw error
+        if ($user_name != "sandipta") {
+          $errorname = "Enter User Name Correctly";
+        }
+        //If  password is wrong throw error
+        if ($password != "innoraft") {
+          $errorpassword = "Enter Password Correctly";
+        }
+        //if username and passwor is correct redirect to action page
+        if ($user_name === "sandipta18" || $user_name === "indra" || $user_name == "rajdip" && $password === "admin") {
+          $_SESSION['name'] = ucwords(strtolower($_POST['Name']));
+          header("Location:action.php");
+        }
+
+      }
+      $output = array($user_name,$password,$errorname,$errorpassword);
+      return $output;
+
+    }
   }
+  ?>
+  <?php
+  $obj = new Login();
+  $temp = $obj->validate_login();
   ?>
   <!-- content -->
   <div class="container flex-wrapper">
     <div class="form-section flex-wrapper -vertical">
       <form action="index.php" class="flex-wrapper -vertical" method="POST">
         <input type="text" placeholder="User Name" name="Name" class="box" required> <span class="error">
-          <?php echo $errorname; ?>
+          <?php echo $temp[2]; ?>
         </span><br>
         <input type="password" placeholder="Password" name="Password" class="box" required><span class="error">
-          <?php echo $errorpassword; ?>
+          <?php echo $temp[3]; ?>
         </span><br>
         <input type="submit" value="Login" class="box-submit" name="submit">
         <input type="submit" value="    Forgot Password" class="box" name="forgot" class="fp">
