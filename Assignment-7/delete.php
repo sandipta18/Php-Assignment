e291e31e5ece55cb5795ff8e5cfe165866fe6436
sandipta18 <sandipta.sardar@innoraft.com>
2023-03-09 10:05:01 +0530

    FT2023-60 : Updated file path .

--- Assignment-7/delete.php
+++ Assignment-7/delete.php
@@ -13,7 +13,7 @@ include 'loadin.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete Account</title>
-    <link rel="stylesheet" href="style8.css">
+    <link rel="stylesheet" href="css/style8.css">
 </head>
 
 <body>
